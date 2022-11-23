b07cd643db4a9cf024f2541031f72fd819169b28
Benjamin Girvan <bengirvan@fb.com>
2022-11-23 12:10:28 +0000

    added cors to server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const userRouter = require('./routes/userRouter')
 
+app.use(cors())
 app.use(express.json())
 
 app.use("/user", userRouter)
