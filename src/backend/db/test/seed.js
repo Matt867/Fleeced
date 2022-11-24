ab7293085726f4c9e45a7412c0e544a6fb5b0f4c
Matthew Sidaway <matthewsidaway@outlook.com>
2022-11-24 15:58:19 +0000

    seeding file with test data

--- src/backend/db/test/seed.js
+++ src/backend/db/test/seed.js
@@ -4,6 +4,91 @@ const { User, Item, Order } = require('../../models/index.js')
 
 const sync = async () => {
     await db.sync({force: true})
+
+    await User.bulkCreate([
+        {
+            username: "Matt",
+            password: "123",
+            email: "m@s.com"
+        },
+        {
+            username: "Ben",
+            password: "123",
+            email: "m@s.com"
+        },
+        {
+            username: "Tobi",
+            password: "123",
+            email: "m@s.com"
+        },
+        {
+            username: "Lucy",
+            password: "123",
+            email: "m@s.com"
+        },
+        {
+            username: "Carlos",
+            password: "123",
+            email: "m@s.com"
+        },
+        {
+            username: "Kareen",
+            password: "123",
+            email: "m@s.com"
+        },
+    ])
+
+    await Item.bulkCreate([
+        {
+            description: "LALALA",
+            price: 4.99,
+            title: "Patagonia Fleece",
+            tag1: "Cool"
+        },
+        {
+            description: "LALALA",
+            price: 4.99,
+            title: "Socks",
+            tag1: "Cool"
+        },
+        {
+            description: "LALALA",
+            price: 4.99,
+            title: "Beanie",
+            tag1: "Cool"
+        },
+        {
+            description: "LALALA",
+            price: 4.99,
+            title: "Hoodie",
+            tag1: "Cool"
+        },
+        {
+            description: "LALALA",
+            price: 4.99,
+            title: "Gloves",
+            tag1: "Cool"
+        },
+        {
+            description: "LALALA",
+            price: 4.99,
+            title: "Trousers",
+            tag1: "Cool"
+        },
+        {
+            description: "LALALA",
+            price: 4.99,
+            title: "Jeans",
+            tag1: "Cool"
+        },
+        {
+            description: "LALALA",
+            price: 4.99,
+            title: "Shirt",
+            tag1: "Cool"
+        },
+    ])
+
 }
 
 sync()
