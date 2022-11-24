855824d0d28781df1e787bf5b98fd283dd810d43
Benjamin Girvan <bengirvan@fb.com>
2022-11-24 11:31:38 +0000

    added items router

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const userRouter = require('./routes/userRouter')
 const orderRouter = require('./routes/orderRouter')
+const itemsRouter = require('./routes/itemsRouter')
 const { propfind } = require('./routes/userRouter')
 
 app.use(express.json())
 
 app.use('/users', userRouter)
 app.use('/order', orderRouter)
+app.use('/item', itemsRouter)
 
 app.listen(3000)
