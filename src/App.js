e9ac8aa2ba1e4b8ff9f75953bc1189dbb7bba09f
Carlos Digan <carlosdigan@fb.com>
2022-11-24 14:54:39 +0000

    Push

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import Login from "./frontend/components/Login";
-import SignUp from "./frontend/components/SignUp";
-import BasketPage from "./frontend/pages/Basket";
-
+import Login from "./frontend/pages/Login";
+import SignUp from "./frontend/pages/SignUp";
+import Home from "./frontend/pages/Home";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path='/basket' element={<BasketPage />} />
       </Routes>
     </BrowserRouter>
   );
