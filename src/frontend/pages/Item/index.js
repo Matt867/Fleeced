6be8fbf4df5b1fec9890ab6d4387083bbbe20a13
Matthew Sidaway <matthewsidaway@outlook.com>
2022-11-25 14:13:23 +0000

    merge

--- src/frontend/pages/Item/index.js
+++ src/frontend/pages/Item/index.js
@@ -1,24 +1,31 @@
+import Header from "./components/Header";
+import SeachBar from "./components/SearchBar";
+import SignUpButton from "./components/SignUpButton";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import ImageCard from "./components/ImageCard";
+import StatsCard from "./components/StatsCard";
 
-// const Item = ({ item, setItem }) => {
-//   return (
-//     <>
-//       <header>
-//         <Header />
-//         <SeachBar />
-//         {loggedIn && <SignUpButton />}
-//         {loggedIn && <LoginButton />}
-//         {!loggedIn && <LogoutButton />}
-//         <Banner />
-//       </header>
-//       <main>
-//         <div>
-//           <ImageCard />
-//         </div>
-//         <div>
-//           <StatsCard />
-//         </div>
-//       </main>
-//     </>
-//   );
-// };
-// export default Item;
+const Item = ({ item, setItem }) => {
+  return (
+    <>
+      <header>
+        <Header />
+        <SeachBar />
+        {loggedIn && <SignUpButton />}
+        {loggedIn && <LoginButton />}
+        {!loggedIn && <LogoutButton />}
+      </header>
+      <main>
+        <div>
+          <ImageCard src={item.src} alt={item.alt} />
+        </div>
+        <div>
+          <StatsCard />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Item;
