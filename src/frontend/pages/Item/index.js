1af96fd12005b53d3c81fcfcdf6afd903526b1ef
Carlos Digan <carlosdigan@fb.com>
2022-11-25 14:04:08 +0000

    Pull

--- src/frontend/pages/Item/index.js
+++ src/frontend/pages/Item/index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 import Header from "./components/Header";
 import SeachBar from "./components/SearchBar";
 import SignUpButton from "./components/SignUpButton";
@@ -30,5 +28,4 @@ const Item = ({ item, setItem }) => {
   );
 };
 
->>>>>>> ef42ca69264ea9d88d6be91d35205972296d49e8
 export default Item;
