ba6c1a61adc8ff1f11407deed6842b8b50738499
misskayghann <kareenghann@live.com>
2022-11-25 17:36:53 +0000

    signup page

--- src/frontend/pages/SignUp/index.js
+++ src/frontend/pages/SignUp/index.js
@@ -1,3 +1,12 @@
+import Header from "../Home/components/Header";
+import NavBar from "../Home/components/NavBar";
+
+import SignUpButton from "../Home/components/SignUpButton"
+import HomeButton from "../Home/components/HomeButton";
+
+import styles_Header from "../Home/components-styles/Header.module.css"
+import Login_styles from "../SignUp/component-styles/SignUp.module.css"
+
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
@@ -7,45 +16,57 @@ const SignUp = () => {
   return (
     <>
       <header>
-        <h1>Fleeced</h1>
-      </header>
-      <main>
-        <h2>Sign Up</h2>
-        <form>
-          <div className="first-name">
-            <label>First Name:</label>
-            <br />
-            <input
-              required
-              type="text" />
+        <Header />
+          <div className={styles_Header.btncontainer}>
+            <SignUpButton />
+            <HomeButton />
           </div>
-          <div className="last-name">
-            <label>Last Name:</label>
-            <br />
-            <input
-              required
-              type="text" />
-          </div>
-          <div className="username">
-            <label>Username:</label>
-            <br />
-            <input
-              required
-              type="text" />
-          </div>
-          <div className="password">
-            <label>Password:</label>
-            <br />
-            <input
-              required
-              type="password" />
-          </div>
+        </header>
+        <section>
+          <NavBar />
+        </section>
+      <main className={Login_styles.main}>
+        <section className={Login_styles.section}>
+          <h2>Sign Up</h2>
+          <form>
+            <section className={Login_styles.info}>
+              <div className="first-name">
+                <label>First Name:*</label>
+                <br />
+                <input
+                  required
+                  type="text" />
+              </div>
+              <div className="last-name">
+                <label>Last Name:*</label>
+                <br />
+                <input
+                  required
+                  type="text" />
+              </div>
+              <div className="username">
+                <label>Username:*</label>
+                <br />
+                <input
+                  required
+                  type="text" />
+              </div>
+              <div className="password">
+                <label>Password:*</label>
+                <br />
+                <input
+                  required
+                  type="password" />
+              </div>
+            </section>
+            
           <br />
           <div className="login-signup">
-            <button type="sumbit">Sign Up</button>
-            <Link to="/login"><button>Log In</button></Link>
+            <button type="sumbit" className={Login_styles.signup}>Sign Up</button>
+            <Link to="/login"><button className= {Login_styles.signin}>Log In</button></Link>
           </div>
         </form>
+        </section>
       </main>
     </>
   )
